046b1ee9eeb3c732f112b047ce9b295fd5677331
PresentJay <presentj94@gmail.com>
2019-10-12 15:19:11 +0900

    jay - sep. 3. pracs day 1

---
+++ 정현재/비타알고 시즌2/09월 1주차_환상의 조합 (★2)/09월 1주차_환상의 조합 (★2)/소스.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int size, object, count = 0;
+	int num[100];
+	cin >> size >> object;
+	for (int i = 0; i < size; i++)
+		cin >> num[i];
+
+
+
+	cout << count;
+}
